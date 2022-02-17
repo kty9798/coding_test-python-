d868346a9411831295cb1d0402ac1927d3f3ab4f
Taeyoon Kim <kty9798@pusan.ac.kr>
2022-02-17 23:12:33 +0900

    change

--- grid_algorithm.py
+++ grid_algorithm.py
@@ -1,13 +1,20 @@
 n = int(input())
 
-
+#16이면 11 6 1 
+path = []
 best = 0
 while n==0:
-    if n%5 ==0:
-        best = n/5
+    if len(path) ==0 and n%5 ==0:
+        best = n//5
         break
+    elif n <3:
+        if len(path) ==0:
+            best = -1
+            break
+        
     else:
-        best = 1
-        break
+        n -= 5
+        path.append(n)
+        best+=1
 
 print(best)
