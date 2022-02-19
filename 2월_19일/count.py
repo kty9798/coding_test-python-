3098dfce984aab728581ccba6238b1d55e3aa1d1
Taeyoon Kim <kty9798@naver.com>
2022-02-20 00:58:07 +0900

    statistic

--- 2월_19일/count.py
+++ 2월_19일/count.py
@@ -1,4 +1,9 @@
+a =[-2 , 1 , 2, 3, 8]
+#빈도수 구하기 위해서
+
 from collections import Counter
+
+
 
 def get_freq(sorted_list):
     result = Counter(sorted_list)
@@ -19,36 +24,6 @@ def get_freq(sorted_list):
         freq_value = same_index #key
         
     return freq_value
-
-n = int(input())
-#lstrip rstrip 개행 없애는것
-list = []
-b = 0
-
-for i in range(n):
-    b = int(input())
-    list.append(b)
-
-list = sorted(list)
-#print(sum(list)//5)
-mean = int(round(sum(list)/n))
-#index가 0 1 2 3 4 즉 len-1/2
-# 0 1-1/2
-index = int((len(list)-1)/2)
-median = list[index]
-freq = get_freq(list)
-length = list[-1] - list[0]
-
-print(mean)
-print(median)
-print(freq)
-print(length)
-
-#print(list)
-
-
-
-
-
-
+    result = Counter(a)
+    #print(result)
 
