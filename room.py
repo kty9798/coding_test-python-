2fb408e751b6034a9937bd82cb9389e58de8c20d
Taeyoon Kim <kty9798@naver.com>
2022-02-26 22:15:13 +0900

    2_26

---
+++ room.py
@@ -0,0 +1,15 @@
+import sys
+
+#n = int(input())
+
+time= [[0]*2 for _ in range(5)]
+
+print(time)
+
+x = [2,4]
+b = (lambda x : x*2)(x)
+print(b)
+
+lamdas =[lambda x: x+10 , lambda x: x+100]
+
+#람다는 함수와 같다 lambda(5) 함수 쓰듯이
