734230dc26bd5ba8548a05c983eb9e918b438870
Taeyoon Kim <kty9798@naver.com>
2022-03-05 16:13:38 +0900

    3_5

--- room.py
+++ room.py
@@ -11,5 +11,6 @@ b = (lambda x : x*2)(x)
 print(b)
 
 lamdas =[lambda x: x+10 , lambda x: x+100]
-
+test =[2 * x for x in range(5)]
+print(test)
 #람다는 함수와 같다 lambda(5) 함수 쓰듯이
---
+++ room_answer.py
@@ -0,0 +1,36 @@
+from importlib import import_module
+import sys 
+
+
+n = int(sys.stdin.readline())
+
+
+time = [[0]* 2 for _ in range(n)]
+
+
+for i in range(n):
+    s , e = map(int , sys.stdin.readline().split())
+    time[i][0] = s   #시작시간
+    time[i][1] = e #끝시간
+
+#sorting을 1번째로 끝 기준 두번째는 
+time.sort(key = lambda x : (x[1] , x[0]))
+
+
+#처음꺼를 1
+result = 1
+finish_time = time[0][1]
+
+del time[0] #첫번쨰 지우기
+
+print(time)
+for i in time:
+    #i[0] 은 시작시간
+    if i[0] >= finish_time:
+        result+=1
+        finish_time = i[1]
+
+
+print(result)
+
+#1-4 5-7 8-11 12-14
