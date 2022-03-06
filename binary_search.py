9edccaf75b5753566cf6d3e0241ea1505d14808e
Taeyoon Kim <kty9798@naver.com>
2022-03-06 19:45:01 +0900

    solve binary_search

---
+++ binary_search.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+
+
+list_n = list(map(int , sys.stdin.readline().split()))
+list_n =sorted(list_n)
+
+m = int(sys.stdin.readline())
+
+list_m = list(map(int , sys.stdin.readline().split()))
+#list_m = sorted(list_m)
+
+#print(list_n)
+#print(list_m)
+
+result = []
+for i in range(len(list_m)):
+    if list_m[i] in list_n:
+        result.append(1)
+    else:
+        result.append(0)
+
+for i in result:
+    print(i)
