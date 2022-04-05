72a099c1eb7f0314d6d87f1e05bf2b2540be07c9
Taeyoon Kim <kty9798@naver.com>
2022-04-05 09:53:26 +0900

    change 4/5

---
+++ machine_learing.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from termcolor import colored
+
+#dataset utils
+#from utils.dataset_utils import ***
+
+def load_processing_mnist(train_ratio , train_batch_szie , test_batch_size):
+    (train_validation_ds , test_ds ) = 
+
+
+
+
+
+
+    def resetter(metrics_objects):
+        pass
+
+
+
+    def training_reporter(epoch , losses_accs , metric_objects , exp_name =None):
+        pass
